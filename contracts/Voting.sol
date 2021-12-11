2c917a55ca6d3b5c416d5d8d4eded2c42954c7ea
Brendan Giberson <brendan.giberson@blackbaud.me>
2021-12-10 23:38:55 -0500

    Finish up initial contract. Needs testing still

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -5,12 +5,17 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Voting is Ownable{
-    uint public currentProposal;
-    mapping(address => bool) public voters; // Map of all users allowed to vote and their vote on the current proposal
-    mapping(uint256 => Proposal) public proposals;
+    bytes32 public currentProposal;
+    uint public votedCount; // how many users have voted on the proposal
+    uint public voterCount; // how many voters exist
+    uint public timeToVote = 1 minutes;
+
+    address [] public voters;
+    mapping(address => bool) public votersVotedMap; // Map of all users allowed to vote and whether or not they voted
+    mapping(bytes32 => Proposal) public proposals;
 
     struct Proposal {
-        uint proposalId; // unique id of proposal
+        bytes32 proposalId; // unique id of proposal
         uint votesFor; // # of voters in favor
         uint vetoVotes; // # of voters against
         uint proposedTimestamp; // The timestamp when proposal was made
@@ -19,24 +24,92 @@ contract Voting is Ownable{
     }
 
     constructor() {
-    }
-
-    function createProposal(string memory proposal) public onlyOwner {
-        // check if current proposal has ended before allowing creation of another
-        require(proposals[currentProposal].isFinished);
-    }
-
-    function isCurrentProposalFinished() public returns(bool) {
-
+        votedCount = 0;
+        voterCount = 0;
+        currentProposal = 0;
     }
 
     /*
-        Owner should be able to make proposals
-        List of voters
-        Once all voters vote, or the time period is reached the vote is over
-        All propasals are stored in an object
-        Each user should only be allowed to vote once per proposal 
-            - the user object will contain a list of all proposals voted on
-        Ability to get vote count for a proposal 
+        Create a new proposal if the previous has finished. 
     */
+    function createProposal(bytes32 proposalUuid, string memory proposal, uint timeToVoteInMins) public onlyOwner {
+        require(isCurrentProposalFinished(), "There is still a proposal underway");
+        require(proposals[currentProposal].proposalId < 0, "The proposal id must be unique");
+
+        proposals[currentProposal] = Proposal({
+            proposalId: proposalUuid,
+            votesFor: 0,
+            vetoVotes: 0,
+            proposedTimestamp: block.timestamp,
+            proposal: proposal,
+            isFinished: false
+        });
+
+        setProposalTime(timeToVoteInMins);
+    }
+
+    /*
+       Change the duration that the vote is alive
+    */
+    function setProposalTime(uint timeInMinutes) public onlyOwner {
+        timeToVote = (timeInMinutes * 1 minutes);
+    }
+
+    /*
+        Give a user voting privledges
+    */
+    function addVoter(address voter) public onlyOwner {
+        voters.push(voter);
+        votersVotedMap[voter] = false;
+        voterCount++;
+    }
+
+    function vote(bool voteToPassProposal) public {
+        require(votersVotedMap[msg.sender] == false, "User has already voted on this proposal");
+        require(!isCurrentProposalFinished(), "There currently isn't anything to vote on");
+
+        if (voteToPassProposal) {
+            proposals[currentProposal].votesFor++;
+        } else {
+            proposals[currentProposal].vetoVotes++;
+        }
+
+        votedCount++;
+        votersVotedMap[msg.sender] = true;
+    }
+    
+    /*
+        Checks if the current proposal has ended.
+        If it has then we reset voted count as well as the votedMapping
+    */
+    function isCurrentProposalFinished() public returns(bool) {
+        // check if all voters have voted and that the proposal hasn't expired
+        if (votedCount != voterCount && (proposals[currentProposal].proposedTimestamp  + timeToVote) < block.timestamp) {
+            return false;
+        } else if (!proposals[currentProposal].isFinished) {
+            proposals[currentProposal].isFinished = true;
+            votedCount = 0;
+
+            // reset all voter to vote map to false
+            for (uint i=0; i< voterCount ; i++){
+                votersVotedMap[voters[i]] = false;
+            }     
+
+            return true;       
+        } else {
+            return true;
+        }
+    }
+
+    function currentVotesFor() external view returns(uint) {
+        return proposals[currentProposal].votesFor;
+    }
+
+    function currentVetosFor() external view returns(uint) {
+        return proposals[currentProposal].vetoVotes;
+    }
+
+    function didProposalPass(bytes32 proposalName) external view returns(bool) {
+        return proposals[proposalName].vetoVotes < proposals[proposalName].votesFor;
+    }
 }
